913b6233d6e08cd14bd3471920b1fb8d9c6202fc
ljalmanzar <ljalmanzar@gmail.com>
2020-05-02 16:08:57 -0700

    adding correct routing using django render. adding templates with a base template. adding bootstrap and custom css for navbar and blog posts.

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+# fake data for now
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018',
+    },
+    {
+        'author': 'Jane',
+        'title': 'Blog Post 2',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018',
+    },
+    {
+        'author': 'Mike',
+        'title': 'Blog Post 3',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018',
+    },
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</hi>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
-    
+    return render(request, 'blog/about.html', { 'title': 'About' })
