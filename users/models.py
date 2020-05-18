567eab3a9578a7328b25df78beabc18c2a25b5b9
ljalmanzar <ljalmanzar@gmail.com>
2020-05-18 15:04:31 -0700

    adding ability to edit user profile and image. adding the user image to the posts.

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -7,3 +8,17 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    # overwriting the save method to add functionality
+    def save(self):
+        # running parent save
+        super().save()
+
+        # openning image & resizing
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            # this goes to the file path and overwrites the image
+            img.save(self.image.path)
--- users/views.py
+++ users/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 def register(request):
     if request.method == 'POST':
+        # if this is a post request, pass the data to the form.
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             # this saves all the form data for us
@@ -15,8 +16,36 @@ def register(request):
             return redirect('login')
     else:
         form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form': form})
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'users/register.html', context)
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        # can give the forms the same data types to autofill information
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        # since the profile update is going to have files in the req, must pass that to the form as well.
+        p_form = ProfileUpdateForm(request.POST,
+                                    request.FILES,
+                                    instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+
+        messages.success(request, f'Account updated.')
+        return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form,
+    }
+    return render(request, 'users/profile.html', context)
