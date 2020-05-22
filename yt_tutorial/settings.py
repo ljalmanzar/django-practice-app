e598e3c361151569a371d06ddf70e8b41cb42aac
ljalmanzar <ljalmanzar@gmail.com>
2020-05-22 16:33:30 -0700

    config change for static root for heroku

--- yt_tutorial/settings.py
+++ yt_tutorial/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # This is config for where to store assets like profile pictures
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -159,3 +160,5 @@ AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_REGION_NAME = 'us-east-2'
 AWS_S3_SIGNATURE_VERSION = 's3v4'
+
+# 
